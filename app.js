fc8d9a36afdfcbb524a955517db366a385cc5f64
arrianne <arrianneoshea@hotmail.co.uk>
2019-06-19 17:22:35 +0100

    creating methods for player options and writing attack logic

--- app.js
+++ app.js
@@ -10,6 +10,42 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack: function() {
+      // Removing health from monster
+      var max = 10;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if(this.monsterHealth <= 0) {
+        alert('you won!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+      // Removing health from player
+      var max = 12;
+      var min = 5;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if(this.playerHealth <= 0) {
+        alert('you lost!');
+        this.gameIsRunning = false;
+        return;
+      }
+
+    },
+    specialAttack: function() {
+
+    },
+    heal: function() {
+
+    },
+    giveUp: function() {
+
     }
   }
+
 });
